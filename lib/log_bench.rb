e002a22515a0297f1b68a4f2482a3cdd505e32e7
Rodrigo Vilina <roanvilina@gmail.com>
2025-06-18 21:20:53 -0400

    Automate gem setup
    With these changes the user will have to do absolutely nothing on
    `config/environments/`

--- lib/log_bench.rb
+++ lib/log_bench.rb
@@ -30,7 +30,29 @@ module LogBench
 
     def setup
       self.configuration ||= Configuration.new
-      yield(configuration)
+      return if @already_setup
+
+      yield(configuration) if block_given?
+      configure_rails_logging if defined?(Rails)
+
+      @already_setup = true
+    end
+
+    private
+
+    def configure_rails_logging
+      Rails.application.configure do
+        config.lograge.enabled = true
+        config.lograge.formatter = Lograge::Formatters::Json.new
+
+        config.lograge.custom_options = lambda do |event|
+          event.payload[:params]&.except("controller", "action")
+            .presence&.then { |p| {params: p} }
+        end
+
+        config.logger ||= ActiveSupport::Logger.new(config.default_log_file)
+        config.logger.formatter = LogBench::JsonFormatter.new
+      end
     end
   end
 end
