125ba712f101461dff4c4934eb027aedf1f0bb65
Rodrigo Vilina <roanvilina@gmail.com>
2025-06-23 12:51:37 -0400

    Extract `LogBench::Parse` into its own file

--- lib/log_bench.rb
+++ lib/log_bench.rb
@@ -12,16 +12,10 @@ loader.setup
 module LogBench
   class Error < StandardError; end
 
+  extend LogBench::Parse
+
   class << self
     attr_accessor :configuration
-
-    # Parse log lines and return an array of entries
-    def parse(lines)
-      lines = [lines] if lines.is_a?(String)
-
-      collection = Log::Collection.new(lines)
-      collection.requests
-    end
 
     def setup
       self.configuration ||= Configuration.new
