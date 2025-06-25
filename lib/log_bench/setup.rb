95dc85f8268c44bd132f3bb68edb0c1280e35a41
Rodrigo Vilina <roanvilina@gmail.com>
2025-06-25 11:27:40 -0400

    Update `LogBench::Setup`
    With this change, we prevent the setup to be called twice.
    We also move all of the setup boilerplate into `LogBench.setup`.

--- lib/log_bench/setup.rb
+++ lib/log_bench/setup.rb
@@ -3,7 +3,29 @@
 module LogBench
   module Setup
     def setup
-      yield(configuration)
+      return if @_already_setup
+
+      yield(configuration) if block_given?
+      configure_rails_logging
+
+      @_already_setup = true
+    end
+
+    def configure_rails_logging
+      return unless defined?(Rails)
+
+      Rails.application.configure do
+        config.lograge.enabled = true
+        config.lograge.formatter = Lograge::Formatters::Json.new
+
+        config.lograge.custom_options = lambda do |event|
+          event.payload[:params]&.except("controller", "action")
+            .presence&.then { |p| {params: p} }
+        end
+
+        config.logger ||= ActiveSupport::Logger.new(config.default_log_file)
+        config.logger.formatter = LogBench::JsonFormatter.new
+      end
     end
   end
 end
